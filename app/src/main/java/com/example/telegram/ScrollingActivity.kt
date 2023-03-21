c824e59f0b22e6c4c986107204b22a7f02555b44
Karrar Abbas <54599574+karrar-abbas@users.noreply.github.com>
2023-03-21 16:33:44 +0300

    first commit

---
+++ app/src/main/java/com/example/telegram/ScrollingActivity.kt
@@ -0,0 +1,24 @@
+package com.example.telegram
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.telegram.databinding.ActivityScrollingBinding
+
+class ScrollingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScrollingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityScrollingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.toolbarLayout.title = title
+
+    }
+}
